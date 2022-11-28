9900dfcc802a2ae8b296b6a557318622632c899b
Suylo <spoownz@yahoo.com>
2022-11-28 23:04:57 +0100

    feat(front): btn design added

---
+++ template.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/prod/css/main.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <title>Composants</title>
+</head>
+<body>
+
+<h1 style="margin: 15px">Boutons</h1>
+<div class="list">
+    <a href="#" class="btn btn-primary">btn-primary</a>
+    <a href="#" class="btn btn-primary-nb">btn-primary-nb</a>
+    <a href="#" class="btn btn-secondary">btn-secondary</a>
+    <a href="#" class="btn btn-secondary-nb">btn-secondary-nb</a>
+</div>
+
+<h1 style="margin: 15px">Boutons avec icônes</h1>
+<div class="list">
+    <a href="#" class="btn btn-primary"><?php include "assets/svg/list.svg" ?>&nbsp;A propos</a>
+    <a href="#" class="btn btn-primary-nb"><?php include "assets/svg/user.svg" ?>&nbsp;Se connecter</a>
+    <a href="#" class="btn btn-secondary"><?php include "assets/svg/medic.svg" ?>&nbsp;Besoin d'aide ?</a>
+    <a href="#" class="btn btn-secondary-nb"><?php include "assets/svg/user_add.svg" ?>&nbsp;S'inscrire</a>
+</div>
+
+<script src="assets/prod/css/main.min.css"></script>
+</body>
+</html>
