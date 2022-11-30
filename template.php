49d73946b7c7c5da996dc03259ce91794c619231
Suylo <spoownz@yahoo.com>
2022-11-30 22:48:27 +0100

    feat(home-page): header & banner

--- template.php
+++ template.php
@@ -5,10 +5,6 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="assets/prod/css/main.min.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap" rel="stylesheet">
     <title>Composants</title>
 </head>
 <body>
