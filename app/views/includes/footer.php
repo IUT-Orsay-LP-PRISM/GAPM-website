16630af2cccb51b6f222f19fcbec3b5869fb88b9
Suylo <spoownz@yahoo.com>
2022-12-02 20:52:33 +0100

    feat(footer): footer design added

---
+++ app/views/includes/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+    <div class="footer__about">
+        <p>A propos de</p>
+        <ul>
+            <li><a href="#">Mentions légales</a></li>
+            <li><a href="#">Nous contacter</a></li>
+            <li><a href="#">Spécialités</a></li>
+            <li><a href="#">Rechercher un praticien</a></li>
+        </ul>
+    </div>
+    <div class="footer__misc">
+        <p>Divers</p>
+        <ul>
+            <li><a href="#">FAQ</a></li>
+            <li><a href="#">CGU</a></li>
+            <li><a href="#">CGV</a></li>
+            <li><a href="#">Politique de confidentialité</a></li>
+        </ul>
+    </div>
+    <div class="footer__social">
+        <p>Suivez-nous</p>
+        <ul>
+            <li><a href="#">Twitter</a></li>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Linkedin</a></li>
+            <li><a href="#">YouTube</a></li>
+        </ul>
+    </div>
+    <div class="footer__access">
+        <p>Accès</p>
+        <ul>
+            <li><a href="#">Mon compte</a></li>
+            <li><a href="#">Devenir praticien</a></li>
+            <li><a href="#">Personnel administratif</a></li>
+            <li><a href="#">Administrateur</a></li>
+        </ul>
+    </div>
+</footer>
+<section class="footer__pre">
+    Projet Sujet 2 G.A.P.M. - M. Gagné • Christopher D, Louis L, Frédéric D., Mathieu M. Valentin, Taisong, Enolah, Joël.
+</section>
