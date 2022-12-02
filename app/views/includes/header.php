15072eac62befbfa51e6e26b036e628984f24b81
Suylo <spoownz@yahoo.com>
2022-12-02 20:52:11 +0100

    feat(includes): header & footer added

---
+++ app/views/includes/header.php
@@ -0,0 +1,19 @@
+<header class="header">
+    <nav class="header__nav transparent">
+        <ul class="header__menu menu-left">
+            <li class="menu-item"><a href="#" class="--active">Accueil</a></li>
+            <li class="menu-item"><a href="#">Besoin d'aide ?</a></li>
+            <li class="menu-item"><a href="#">A propos</a></li>
+        </ul>
+        <ul class="header__menu menu-right">
+            <li class="menu-item"><a href="#" class="btn btn-primary-nb">
+                    <?php include "assets/svg/user.svg" ?>&nbsp;Se
+                    connecter
+                </a></li>
+            <li class="menu-item"><a href="#" class="btn btn-secondary-nb">
+                    <?php include "assets/svg/user_add.svg" ?>
+                    &nbsp;S'inscrire
+                </a></li>
+        </ul>
+    </nav>
+</header>
