d5df1bcbf7a260af842d476ccd70fcee85885d8a
Suylo <spoownz@yahoo.com>
2022-12-03 14:46:33 +0100

    fix(all): misc

--- app/views/includes/header.php
+++ app/views/includes/header.php
@@ -6,7 +6,7 @@
             <li class="menu-item"><a href="#">A propos</a></li>
         </ul>
         <ul class="header__menu menu-right">
-            <li class="menu-item"><a id="btn-connexion" href="#" class="btn btn-primary-nb">
+            <li class="menu-item"><a id="btn-connexion" href="javascript:void()" class="btn btn-primary-nb">
                     <?php include "assets/svg/user.svg" ?>&nbsp;Se
                     connecter
                 </a></li>
