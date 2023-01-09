4e4cc9167f26c96cc45e60a8a1ddb6cea74a8aac
Suylo <spoownz@yahoo.com>
2023-01-09 22:18:05 +0100

    feat(search): merde j'ai oublié ça

--- app/models/DAO/demandeurDao.php
+++ app/models/DAO/demandeurDao.php
@@ -3,8 +3,22 @@
 namespace App\models\dao;
 
 use App\models\entity\Demandeur;
+use PDO;
 
 class DemandeurDAO extends ConnexionDB{
 
     protected static $entity = "Demandeur";
+    protected static $link = 'App\models\entity\\';
+
+    public static function findByNameOrCity($nom, $city)
+    {
+        $sql = "SELECT * FROM demandeur WHERE nom LIKE :nom OR prenom LIKE :city";
+        $stmt = self::getInstance()->prepare($sql);
+        $stmt->execute([
+            'nom' => "%$nom%",
+            'city' => "%$city%"
+        ]);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, static::$link . static::$entity);
+        return $stmt->fetchAll();
+    }
 }
