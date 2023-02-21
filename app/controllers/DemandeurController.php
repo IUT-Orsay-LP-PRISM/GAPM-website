0d70024c61f13b392c96d119dc1a6ec19667498b
v4nkor <vanknpix@gmail.com>
2023-02-21 08:54:34 +0100

    Working inscription + added Sexe select to inscription form

--- app/controllers/DemandeurController.php
+++ app/controllers/DemandeurController.php
@@ -68,9 +68,9 @@ abstract class DemandeurController extends Template implements InterfaceControll
         $birthday = $_POST['birthday'];
         $password = $_POST['password'];
         $city = $_POST['city'];
-        $cp = $_POST['cp'];
         $phone = $_POST['phone'];
         $address = $_POST['address'];
+        $sexe = $_POST['sexe'];
 
         $salt= "sel";
         $saltedAndHashed = crypt($password,$salt);
@@ -83,16 +83,12 @@ abstract class DemandeurController extends Template implements InterfaceControll
         $demandeur->setIdVille($city);
         $demandeur->setMotDePasse($saltedAndHashed);
         $demandeur->setTelephone($phone);
-        $demandeur->setSexe("M");
+        $demandeur->setSexe($sexe);
 
         dump($demandeur);
-        //$demandeur = DemandeurDAO::create($demandeur);
+        $demandeur = DemandeurDAO::create($demandeur);
 
         // TODO FOR BDD:
         // - LOGIN inutile
-        // - COMMENT ON GET LE SEXE ?
-        // POUR PUSH TELEPHONE IL FAUT LE METTRE au début en +33 ?
-        // ici on set pas le code postal car il est pas dans la table demandeur il faudrai car 1 ville peut avoir plusieurs cp
-        // donc il faudrai faire une table intermédiaire entre ville et cp
     }
 }
--- app/views/includes/popUpInscription.php
+++ app/views/includes/popUpInscription.php
@@ -33,6 +33,14 @@
             </div>
             <div class="popup-container-row">
                 <div class="input-container">
+                    <select id="sexePopUp" class="input" type="text" name="sexe" required>
+                        <option value="M">Homme</option>
+                        <option value="F">Femme</option>
+                        <option value="A">Autre</option>
+                    </select>
+                    <label class="label" for="telPopUp">Sexe</label>
+                </div>
+                <div class="input-container">
                     <i class="icon icon-fi-rr-home-location-alt"></i>
                     <input id="adressPopUp" class="input" type="text" name="address" required/>
                     <label class="label" for="adressPopUp">Votre adresse (Numéro, Rue)</label>
@@ -55,7 +63,7 @@
             <div class="popup-container-row">
                 <div class="input-container">
                     <i class="icon icon-fi-rr-key"></i>
-                    <input id="mdp-inscPopUp" class="input" type="text" name="password" required/>
+                    <input id="mdp-inscPopUp" class="input" type="password" name="password" required/>
                     <label class="label" for="mdp-inscPopUp">Votre mot de passe</label>
                 </div>
                 <div class="input-container">
