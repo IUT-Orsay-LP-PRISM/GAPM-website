b0682d914e786a96f297aef5558572a8676bb59c
Fredray-21 <dabadie.frederic@gmail.com>
2023-02-26 00:33:17 +0100

    feat(prendre-RDV): add verif Logged if bypass

--- app/controllers/RendezVousController.php
+++ app/controllers/RendezVousController.php
@@ -3,11 +3,17 @@
 namespace App\controllers;
 
 use App\models\dao\DemandeurDAO;
+use App\models\entity\Session;
 
 abstract class RendezVousController extends Template implements InterfaceController
 {
     public static function prendreRdv()
     {
+        if(Session::isLogged() == false) {
+            header('Location: /?action=search&s_name=&s_city=&error=Pour prendre rendez-vous, veuillez vous identifier&c=connexion');
+            exit;
+        }
+
         $intervenant = DemandeurDAO::findById($_GET['intervenant']);
 
         self::render('demandeur/search/prendre-rdv.twig', [
