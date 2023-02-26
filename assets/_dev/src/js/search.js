def32595fa7f229380ca09e45726a7a6512a9284
Fredray-21 <dabadie.frederic@gmail.com>
2023-02-26 16:12:08 +0100

    fix(prendre-RDV) : css select

--- assets/_dev/src/js/search.js
+++ assets/_dev/src/js/search.js
@@ -1,4 +1,5 @@
 const btn_RDVS = document.querySelectorAll('.js-btn-RDV');
+const popUp_prendreRDV = document.querySelector('#popUp-prendre-RDV');
 
 if (btn_RDVS) {
     btn_RDVS.forEach(btn => {
@@ -7,7 +8,6 @@ if (btn_RDVS) {
             const idDemandeur = btn.getAttribute('data-id');
             const isLogged = document.body.getAttribute('data-log');
             if (isLogged != 1) {
-                const currentUrl = window.location.href;
                 const currentQueryString = window.location.search;
                 const newUrl = currentQueryString + '&error=Pour prendre rendez-vous, veuillez vous identifier&c=connexion';
                 window.location.href = newUrl;
@@ -151,7 +151,6 @@ function StartCalendar() {
 
 
 function placeEventListenerInDays() {
-    const popUp_prendreRDV = document.querySelector('#popUp-prendre-RDV');
     document.querySelectorAll('.day:not(.--disabled)').forEach(divDay => {
         divDay.addEventListener('click', () => {
             popUp_prendreRDV.classList.toggle('visible');
@@ -165,4 +164,3 @@ function placeEventListenerInDays() {
         });
     });
 }
-
