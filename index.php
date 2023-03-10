5dbf104b8068b6cd4172817cd0923af88d447dcb
Suylo <spoownz@yahoo.com>
2023-03-10 23:54:40 +0100

    feat(mes-rdv): Ajout css début

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ Route::get('logout', 'DemandeurController', 'logout');
 Route::get('my-account', 'DemandeurController', 'displayMyAccount');
 Route::get('prendre-rdv', 'RendezVousController', 'index');
 Route::get('success-rdv', 'RendezVousController', 'success');
+Route::get('mes-rendez-vous', 'RendezVousController', 'displayMyRdv');
 
 Route::post('login-user', 'DemandeurController', 'login');
 Route::post('register-user', 'DemandeurController', 'register');
